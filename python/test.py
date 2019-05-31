a669e7c2560eb3c000ecd59d06d41b760a1f3e86
Emeric Fontaine <emeric.fontaine@orange.com>
2019-05-31 22:45:03 +0200

    clean some test

--- python/test.py
+++ python/test.py
@@ -12,20 +12,6 @@ class Test(unittest.TestCase):
         run.compute_input(2)
         self.assertEqual(1, 2)
 
-    def test_should_number_work(self):
-        run.compute_input(2)
-
-    def test_should_be_number(self):
-        try:
-            run.compute_input("truc")
-        except Exception as e:
-            self.assertTrue(True)
-            print(e)
-        else:
-            self.assertTrue(False)
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
