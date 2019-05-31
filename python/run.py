c9801cdc463e79b35be6c6f268bf1fcdd1ac5d65
Emeric Fontaine <emeric.fontaine@orange.com>
2019-05-31 15:22:25 +0200

    first test

--- python/run.py
+++ python/run.py
@@ -1,3 +1,8 @@
-age = input("What is your age: ")
 
-print("Age = %d" %age)
+def compute_input(age):
+    print("Age = %d" %age)
+
+if __name__ == "__main__":
+    age = input("What is your age: ")
+    compute_input(int(age))
+
--- python/test.py
+++ python/test.py
@@ -4,11 +4,28 @@
     python -m unittest test.TestSequenceFunctions.test_getTaxRate
 """
 import unittest
+import run
 
 class Test(unittest.TestCase):
 
     def test_env(self):
+        run.compute_input(2)
         self.assertEqual(1, 2)
+
+    def test_should_number_work(self):
+        run.compute_input(2)
+
+    def test_should_be_number(self):
+        try:
+            run.compute_input("truc")
+        except Exception as e:
+            self.assertTrue(True)
+            print(e)
+        else:
+            self.assertTrue(False)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
